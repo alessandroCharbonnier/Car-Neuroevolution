a94e54f302ca2eb02ff7437ce54738cc06e86304
alessandrocharbonnier <haloalessandro230@gmail.com>
2019-04-27 19:21:57 +0200

    added red crosses

--- js/eye.js
+++ js/eye.js
@@ -15,5 +15,20 @@ class Eye {
         noFill();
         stroke(255, this.alpha);
         line(this.x1, this.y1, this.x2, this.y2);
+        this.drawCross(this.x2, this.y2, 15);
+    }
+
+    drawCross(x, y, w) {
+        noStroke();
+        fill(255, 0, 0);
+
+        push();
+        translate(x, y);
+        rotate(-HALF_PI);
+
+        rect(0, 0, w, w * 0.2);
+        rotate(HALF_PI);
+        rect(0, 0, w, w * 0.2);
+        pop();
     }
 }
